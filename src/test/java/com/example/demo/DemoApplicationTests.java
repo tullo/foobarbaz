7f02c475f2f3f8804b4da40b0d659fea447de1d8
Andreas Amstutz <tullo@users.noreply.github.com>
2022-09-07 20:22:33 +0200

    print env vars

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Map<String, String> env = System.getenv();
+		env.forEach((k, v) -> System.out.println(k + ":" + v));
 	}
 
 }
